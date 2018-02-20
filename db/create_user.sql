2e5eee7f2dec5d8e792305ddea51994ed3bc91ac
PowellBrand <powellbrandon@outlook.com>
2018-02-20 14:12:49 -0700

    find_user

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users 
 (name, authID, hp, mana, avatar, list, class, lvl, gold, message)
 values
-($1, $2, 50, 50, $3, $4, $5, $6, $7, $8)
+($1, $2, 50, 50, $3, $4, $5, 1, 0, $8)
 returning *;
