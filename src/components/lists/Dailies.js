b6ad36176ca6fbc72a209c0718ab206d6be5547f
Wheatley <tiarra.stout@gmail.com>
2018-02-23 12:57:43 -0700

    daily

--- src/components/lists/Dailies.js
+++ src/components/lists/Dailies.js
@@ -1,21 +1,57 @@
 import React, {Component} from 'react';
+import {getLists, addDailies} from '../../ducks/reducer';
+import { connect } from 'react-redux';
+
 
 class Dailies extends Component {
     constructor(){
         super()
+        this.state={
+            content: ''
+        }
     }
 
     componentWillMount(){
-        //pull all dailies
+        this.props.getLists()
+    }
+
+    content(string){
+        this.setState({
+            content: string
+        })
     }
 
     render(){
+
+        let dailies = this.props.lists.map(item=>{
+            if (item.daily_todo==="daily"){
+                return (
+                    <div key={item.id}>{item.content}</div>
+                )
+            }
+        })
+
         return(
             <div>
-                <form>
-                <input placeholder="Add a daily here"/>
+                <form onSubmit={(e)=>{
+                    e.preventDefault();
+                    this.props.addDailies(this.state.content)}}>
+                <input placeholder="Add a daily here" onChange={e=> {
+                this.content(e.target.value);
+                }}/>
+                <button type="submit">Submit</button>
                 </form>
+                {dailies}
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return{
+        lists: state.lists,
+        addedDaily : state.daily
+    }
+}
+
+export default connect(mapStateToProps, {getLists, addDailies})(Dailies)
