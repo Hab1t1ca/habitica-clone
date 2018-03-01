16d7393c22e916ac031c214f0531b4fd58a34dbf
Dante Dufala <thelonelycomet@gmail.com>
2018-03-01 09:21:41 -0700

    stuff

--- src/components/userIcon/UserIcon.js
+++ src/components/userIcon/UserIcon.js
@@ -80,9 +80,9 @@ class UserIcon extends Component {
                 <div className="Avatar">
                 <img  className="avatarWindow" src={this.props.user.avatar}/>
                 <img className="stickmanInBox" src={stickman}/>
-                <img className="WeaponRightHand" src="http://res.cloudinary.com/rigrater/image/upload/a_355/v1519841832/EnergySword_sclzal.png"/>
+                <img className="WeaponRightHand" src="http://res.cloudinary.com/rigrater/image/upload/v1519798103/RainbowSword_tfvtkb.png"/>
                 <img className="chestArmor" src="http://res.cloudinary.com/rigrater/image/upload/c_scale,w_30/v1519777195/policeVest.png"/>
-                <img className="hat" src="http://res.cloudinary.com/rigrater/image/upload/c_scale,w_35/v1519806733/DevMountainHat_znua1u.png"/>
+                <img className="hat" src="http://res.cloudinary.com/rigrater/image/upload/c_scale,w_40/v1519806733/DevMountainHat_znua1u.png"/>
                 </div>
 
 
@@ -92,9 +92,9 @@ class UserIcon extends Component {
                 <div className="stats">
                     <br/>
                     <div className="Class">
-                    
+                  
                     {this.props.user.class}
-
+                    <br/>
                 <div className="Golds">
                     <img src={gold} className="Gold"/>
                     {this.props.user.gold}
