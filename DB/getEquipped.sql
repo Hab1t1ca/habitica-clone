0c9db7646c4c6de2a6de9c62965a14f4d8de7b5c
Mason Brown <mbrown9615@gmail.com>
2018-03-03 16:39:20 -0500

    thing

--- DB/getEquipped.sql
+++ DB/getEquipped.sql
@@ -1,3 +1,11 @@
 
+
 SELECT "equipped" from users
 WHERE userid = $1;
+
+-- SELECT * FROM items JOIN users
+-- WHERE items.itemid = users.equipped
+
+-- SELECT t.*
+-- FROM unnest(ARRAY[1,2,3,2,3,5]) item_id
+-- LEFT JOIN items t on t.id=item_id
